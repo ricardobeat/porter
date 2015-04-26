21941da78fbd7ab7d6a8190a4b54c327997f7717
Ricardo Tomasi <ricardobeat@gmail.com>
2015-04-26 21:04:22 +0200

    accept DIR variables from environment

--- lib/boot.sh
+++ lib/boot.sh
@@ -1,14 +1,13 @@
 
-PORTER_DIR="$HOME/.porter"
-PORTER_BIN_DIR="$HOME/bin"
-PORTER_CACHE_DIR="$HOME/.porter/.cache"
+PORTER_DIR="${PORTER_DIR:-HOME/.porter}"
+PORTER_BIN_DIR="${PORTER_BIN_DIR:-HOME/bin}"
+
+PORTER_CACHE_DIR="$PORTER_DIR/.cache"
 
 mkdir -p "$PORTER_DIR"
 mkdir -p "$PORTER_CACHE_DIR"
 
 touch "$PORTER_DIR/porter.conf"
 
-# 
-if [ ! -t 0 -o ! -t 1 ]; then
-  NO_COLOR="1"
-fi
+# Disable colors if not a terminal
+if [ ! -t 0 -o ! -t 1 ]; then NO_COLOR="1"; fi
