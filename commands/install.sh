f48f2e8aa0b755f5feeedd217a01d90729a032e6
Ricardo Tomasi <ricardobeat@gmail.com>
2015-04-26 19:21:21 +0200

    print success message for dependencies

--- commands/install.sh
+++ commands/install.sh
@@ -1,7 +1,13 @@
 
 do_install () {
   source $LIB/package-reader.sh
-  source $LIB/package-methods.sh
-  # Execute package formula
+  
+  # Source package formula to fetch metadata and install method
   source $PACKAGE.sh
+
+  # Load package helpers
+  source $LIB/package-methods.sh
+
+  # Run install() as defined by the package
+  install
 }
--- lib/package-methods.sh
+++ lib/package-methods.sh
@@ -6,6 +6,7 @@ install_bin () {
 requires_bin () {
   local BIN="$1"
   if type $1 &> /dev/null; then
+    _echo green "requires $1: ok"
     return 0
   else
     return 1
@@ -13,11 +14,23 @@ requires_bin () {
 }
 
 requires_brew () {
-  requires_bin 'brew' || die 'Homebrew is not installed. See http://brew.sh'
+  # Check if homebrew is installed
+  if ! type brew &> /dev/null; then
+    die "Homebrew is not available in this system (http://brew.sh)"
+  fi
+
+  # Install formula (if not already installed)
   if ! brew list -1 | grep -q $1; then
     brew install $1
     if [[ $? != 0 ]]; then
       die "Failed to install homebrew formula for $1"
     fi
   fi
+
+  _echo green "requires brew formula $1: ok"
+  return 0
+}
+
+requires_yum () {
+  die "not implemented"
 }
